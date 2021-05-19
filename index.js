d3dc2fee498ed6936135b4db1301915964e314e5
shivasankari3101 <shivasankari3101@gmail.com>
2021-05-19 20:41:41 +0530

    Completed responsive header

---
+++ index.js
@@ -0,0 +1,5 @@
+function open_menu(){
+     document.getElementsByClassName("header_3")[0].classList.toggle("active");
+    document.getElementsByClassName("bars")[0].classList.toggle("cross");
+    document.body.classList.toggle("fixed");
+}
