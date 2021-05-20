602228aa1b9897ae280691ff9230134dea3e97d5
shivasankari3101 <shivasankari3101@gmail.com>
2021-05-20 23:29:55 +0530

    Navigation added

--- js/ajax.js
+++ js/ajax.js
@@ -4,7 +4,27 @@ xhr.onreadystatechange = function(){
     if(this.status==200 && this.readyState==4){
         document.getElementsByClassName("main_content")[0].innerHTML = this.responseText;
     }
+    else{
+        console.log("file not found");
+    }
 }
 
-xhr.open("GET","./snippets/home.html",true);
-xhr.send();
+function open_home(){   
+    xhr.open("GET","./snippets/home.html",true);
+    xhr.send();
+ }
+
+function open_about(){   
+   xhr.open("GET","./snippets/about.html",true);
+   xhr.send();
+}
+
+function open_donutsmenu(){ 
+    xhr.open("GET","./snippets/menu.html",true);
+    xhr.send();
+ }
+
+ function open_contact(){   
+    xhr.open("GET","./snippets/contact.html",true);
+    xhr.send();
+ }
