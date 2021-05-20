fc4754f0c81c056ff176acedd2e0ecd8bdfe767e
shivasankari3101 <shivasankari3101@gmail.com>
2021-05-20 22:34:55 +0530

    Minor changes in homepage

--- js/ajax.js
+++ js/ajax.js
@@ -4,9 +4,6 @@ xhr.onreadystatechange = function(){
     if(this.status==200 && this.readyState==4){
         document.getElementsByClassName("main_content")[0].innerHTML = this.responseText;
     }
-    else{
-        console.log("file not found");
-    }
 }
 
 xhr.open("GET","./snippets/home.html",true);
