94581ebcd52a6937a86879313acdfe3eb6a58d22
shivasankari3101 <shivasankari3101@gmail.com>
2021-05-24 17:50:18 +0530

    Added links and alert

--- data/menu.js
+++ data/menu.js
@@ -3,50 +3,51 @@ var menu_items= {
         {
             "id":1,
             "name":"Divine Crush",
-            "price":"₹4",
+            "price":"₹400",
             "image":"./images/menu/Divine_crush.jpg"
         },
 
         {
             "id":2,
             "name":"Double trouble",
-            "price":"₹3",
+            "price":"₹300",
             "image":"./images/menu/Double_trouble.jpg"
         },
 
         {
             "id":3,
             "name":"Midnight beauty",
-            "price":"₹3",
+            "price":"₹300",
             "image":"./images/menu/Midnight_beauty.jpg"
         },
 
         {
             "id":4,
-            "name":"My Original Donut",
-            "price":"₹3",
-            "image":"./images/menu/My_original_Donut.jpg"
-        },
-
-        {
-            "id":5,
             "name":"Original Sun",
-            "price":"₹3",
+            "price":"₹500",
             "image":"./images/menu/Original_sun.jpg"
         },
 
         {
-            "id":6,
+            "id":5,
             "name":"Rainbow surprise",
-            "price":"₹3",
+            "price":"₹300",
             "image":"./images/menu/Rainbow_surprise.jpg"
         },
 
         {
-            "id":7,
+            "id":6,
             "name":"Sprinkled donuts",
-            "price":"₹3",
+            "price":"₹300",
             "image":"./images/menu/Sprinkled_donuts.jpg"
-        }
+        },
+
+        {
+            "id":7,
+            "name":"My Original Donut",
+            "price":"₹200",
+            "image":"./images/menu/My_Original_donut.jpg"
+            
+        },
     ]
 }
